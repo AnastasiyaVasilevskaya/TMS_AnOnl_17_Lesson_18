eae803b6003008871a289771e2111862f935a936
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-02-18 16:56:18 +0300

    delayed start (Handler, postDelayed)

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.first
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val nextButton: Button = findViewById(R.id.nextButton)
         nextButton.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }, 3000)
         }
     }
 }
