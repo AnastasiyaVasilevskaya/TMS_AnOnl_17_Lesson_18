2f17fd2c16a2bebc0d2b74e68f4cffacd350dd71
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-03-01 16:36:27 +0300

    validation

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -1,25 +1,107 @@
 package com.example.first
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import com.example.first.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        emailTextWatcher()
+        passwordTextWatcher()
 
-        val nextButton: Button = findViewById(R.id.next_button)
-        nextButton.setOnClickListener {
-            val handler = Handler(Looper.getMainLooper())
-            handler.postDelayed({
+        binding.nextButton.setOnClickListener() {
+            val validEmail = binding.emailContainer.helperText == "Correct"
+            val validPassword = binding.passwordContainer.helperText == "Correct"
+
+            if (validEmail && validPassword) {
                 val intent = Intent(this, SecondActivity::class.java)
                 startActivity(intent)
-            }, 3000)
+            }
+
         }
     }
+
+    //Валидация почты
+    private fun emailTextWatcher() {
+        binding.emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                emailHelperText()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+
+    private fun validEmail(): String {
+        val enteredEmail = binding.emailEditText.text.toString().trim()
+        if (!Patterns.EMAIL_ADDRESS.matcher(enteredEmail).matches()) {
+            return "Invalid email"
+        }
+        return "Correct"
+    }
+
+    private fun emailHelperText() {
+        binding.emailContainer.helperText = validEmail()
+
+        if (validEmail() == "Correct") {
+            val colorGreen = ContextCompat.getColor(this, R.color.green)
+            binding.emailContainer.setHelperTextColor(ColorStateList.valueOf(colorGreen))
+        } else {
+            val colorRed = ContextCompat.getColor(this, R.color.red)
+            binding.emailContainer.setHelperTextColor(ColorStateList.valueOf(colorRed))
+        }
+    }
+
+
+    //Валидация пароля
+    private fun passwordTextWatcher() {
+        binding.passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                passwordHelperText()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun validPassword(): String {
+        val passwordText = binding.passwordEditText.text.toString().trim()
+        if (passwordText.length < 6) {
+            return "Invalid password. Minimum 6 characters"
+        }
+        return "Correct"
+    }
+
+    private fun passwordHelperText() {
+        binding.passwordContainer.helperText = validPassword()
+
+        if (validPassword() == "Correct") {
+            val colorGreen = ContextCompat.getColor(this, R.color.green)
+            binding.passwordContainer.setHelperTextColor(ColorStateList.valueOf(colorGreen))
+        } else {
+            val colorRed = ContextCompat.getColor(this, R.color.red)
+            binding.passwordContainer.setHelperTextColor(ColorStateList.valueOf(colorRed))
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/first/SecondActivity.kt
+++ app/src/main/java/com/example/first/SecondActivity.kt
@@ -15,16 +15,5 @@ class SecondActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.back_button).setOnClickListener {
             finish()
         }
-
-
-        findViewById<Button>(R.id.button_to_third).setOnClickListener {
-            val intent = Intent(this, ThirdActivity::class.java)
-            startActivity(intent)
-        }
-
-        findViewById<Button>(R.id.button_to_fourth).setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
